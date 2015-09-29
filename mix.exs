9157dba4a1439d74a3174195cd425863411c62e8
Ross Jones <ross@servercode.co.uk>
2015-09-29 07:46:41 +0100

    0.0.2, with added functions

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CKAN.Mixfile do
 
   def project do
     [app: :ckan,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
